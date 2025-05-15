a0a89a1f7c8f286880d23ac376263d7703500b5f
otaviov <amandavv32@gmail.com>
2025-05-15 16:58:36 -0300

    Adicionando animação teste do aplicativo

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useCallback, useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import {
   FlatList,
   StyleSheet,
---
+++ app/screens/IntroScreen.js
@@ -0,0 +1,42 @@
+// app/screens/IntroScreen.js
+import { useNavigation } from '@react-navigation/native';
+import LottieView from 'lottie-react-native';
+import { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default function IntroScreen() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('Home'); // ou qualquer que seja a tela principal
+    }, 4000); // duração da animação antes de ir pra Home
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+      <View style={styles.container}>
+      <LottieView
+        source={require('../animations/intro.json')}
+        autoPlay
+        loop={false}
+        style={styles.animation}
+      />
+    </View>
+     
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000', // ou branco, ou o que quiser
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  animation: {
+    width: 300,
+    height: 300,
+  },
+});
